563ca30555516156ddde1d8a48b68ba1beb98bde
ngohainnam <ngohainnam@gmail.com>
2024-05-10 18:11:27 +1000

    Upload

---
+++ Command.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group01RestaurantSystem
+{
+    public abstract class Command
+    {
+        private int userChoice;
+        private List<string> commandList;
+        public List<string> CommandList { get => commandList; set => commandList = value; }
+        public Command(List<string> commandList)
+        {
+            CommandList = commandList;
+        }
+
+        public abstract void Execute();
+        public void PrintCommand()
+        {
+            for (int i = 0; i < CommandList.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {CommandList[i]}");
+            }
+            Console.WriteLine("Please enter your choice: ");
+            //check for inval input
+            userChoice = Convert.ToInt32(Console.ReadLine());
+            while (!int.TryParse(Console.ReadLine(), out userChoice) || userChoice < 1 || userChoice > CommandList.Count)
+            {
+                Console.WriteLine("Invalid input, please enter a number between 1 and " + CommandList.Count);
+                userChoice = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserNamespace
+{
+    public abstract class User
+    {
+        private string _fName;
+        private string _fPassword;
+        private string _fEmail;
+        private string _fDateOfBirth;
+
+        // Constructor for derived classes
+        protected User(string name, string password, string email, string dateOfBirth)
+        {
+            _fName = name;
+            _fPassword = password;
+            _fEmail = email;
+            _fDateOfBirth = dateOfBirth;
+        }
+
+        // Abstract method to be implemented by derived classes
+        public abstract void DisplayInfo();
+
+        public string Name
+        {
+            get { return _fName; }
+            protected set { _fName = value; }
+        }
+
+        public string Password
+        {
+            get { return _fPassword; }
+            set { _fPassword = value; }
+        }
+
+        public string Email
+        {
+            get { return _fEmail; }
+            set { _fEmail = value; }
+        }
+
+        public string DateOfBirth
+        {
+            get { return _fDateOfBirth; }
+            set { _fDateOfBirth = value; }
+        }
+    }
+}
+
