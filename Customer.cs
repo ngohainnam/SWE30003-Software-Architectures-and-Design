3cd540ced2aef829e1ecd638433189763e1778ea
ngohainnam <ngohainnam@gmail.com>
2024-05-12 17:27:05 +1000

    Update customer.cs

--- Customer.cs
+++ Customer.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace Group01RestaurantSystem
 {
-    internal class Customer
+    public class Customer : User
     {
+        private string _address;
+        private string _phoneNumber;
+
+        // Constructor
+        public Customer(string name, string password, string email, string dateOfBirth, string address, string phoneNumber)
+            : base(name, password, email, dateOfBirth)
+        {
+            _address = address;
+            _phoneNumber = phoneNumber;
+        }
+
+        public string Address
+        {
+            get
+            {
+                return _address;
+            }
+            set
+            {
+                _address = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+
+        // Implementing the abstract method from the base class
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Customer Information:\n" +
+                              $"Name: {Name}\n" +
+                              $"Email: {Email}\n" +
+                              $"Date of Birth: {DateOfBirth}\n" +
+                              $"Address: {Address}\n" +
+                              $"Phone Number: {PhoneNumber}\n");
+        }
     }
 }
+
--- User.cs
+++ User.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserNamespace
+namespace Group01RestaurantSystem
 {
     public abstract class User
     {
